2c8793ea0c2df88be68a4b2df55113a26aa7c330
Tristan Penman <tristan@tristanpenman.com>
2020-02-16 15:57:53 +1100

    Add incomplete hw2 files

---
+++ hw2/LogAnalysis.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage = undefined
+
+parse :: String -> [LogMessage]
+parse = undefined
+
