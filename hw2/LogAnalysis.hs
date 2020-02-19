9851028689b6eaedbab0ac748532a1359336a7bc
Tristan Penman <tristan@tristanpenman.com>
2020-02-19 23:08:09 +1100

    Add solution for exercise 1 in week 2

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -4,9 +4,19 @@ module LogAnalysis where
 
 import Log
 
+--
+-- Exercise 1
+--
+
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage message = parseMessageParts (words message)
+
+parseMessageParts :: [String] -> LogMessage
+parseMessageParts ("E":severity:timestamp:parts) = LogMessage (Error $ read severity) (read timestamp) (unwords parts)
+parseMessageParts ("I":timestamp:parts) = LogMessage Info (read timestamp) (unwords parts)
+parseMessageParts ("W":timestamp:parts) = LogMessage Warning (read timestamp) (unwords parts)
+parseMessageParts parts = Unknown (unwords parts)
 
 parse :: String -> [LogMessage]
-parse = undefined
+parse messages = map parseMessage (lines messages)
 
