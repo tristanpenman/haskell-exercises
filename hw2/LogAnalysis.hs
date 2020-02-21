8ba23d2e3edb055d605e07643a281ea181d86f08
Tristan Penman <tristan@tristanpenman.com>
2020-02-21 14:24:54 +1100

    Add solution for exercise 2 in week 2

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -7,6 +7,8 @@ import Log
 --
 -- Exercise 1
 --
+-- Parse a series of log messages
+--
 
 parseMessage :: String -> LogMessage
 parseMessage message = parseMessageParts (words message)
@@ -20,3 +22,18 @@ parseMessageParts parts = Unknown (unwords parts)
 parse :: String -> [LogMessage]
 parse messages = map parseMessage (lines messages)
 
+--
+-- Exercise 2
+--
+-- Insert a LogMessage into a MessageTree, giving a new MessageTree containing the message
+--
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert message Leaf = Node Leaf message Leaf
+insert message tree =
+  let (LogMessage _ ts1 _) = message
+      (Node left middle right) = tree
+      (LogMessage _ ts2 _) = middle
+  in if ts1 < ts2 then (Node (insert message left) middle right)
+     else (Node left middle (insert message right))
