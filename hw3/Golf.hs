89c38421ae03c8f3a287d32196be4beb7f281fa8
Tristan Penman <tristan@tristanpenman.com>
2020-02-23 21:22:43 +1100

    Add solution for exercise 3 in week 3

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -71,3 +71,42 @@ localMaxima (x:y:z:xs)
   | x < y && y > z = y : localMaxima (y:z:xs)
   | otherwise      = localMaxima (y:z:xs)
 localMaxima _ = []
+
+--
+-- Exercise 3 - Histogram
+--
+-- Write a function which takes as input a list of Integers between 0 and 9
+-- (inclusive), and outputs a vertical histogram showing how many of each
+-- number were in the input list. You may assume that the input list does not
+-- contain any numbers less than zero or greater than 9 (that is, it does not
+-- matter what your function does if the input does contain such numbers).
+--
+-- Examples:
+--
+-- histogram [1,1,1,5] ==
+--  *
+--  *
+--  *   *
+-- ==========
+-- 0123456789
+--
+--
+-- histogram [1,4,5,4,6,6,3,4,2,4,9] ==
+--     *
+--     *
+--     * *
+--  ******  *
+-- ==========
+-- 0123456789
+
+histogram :: [Integer] -> String
+histogram xs = unlines (rows ++ footer)
+  where counts = tally xs
+        greatest = maximum counts
+        row n = [if (counts !! i >= n) then '*' else ' ' | i <- [0..9]]
+        rows = [row i | i <- [greatest, (greatest - 1)..1]]
+        footer = ["==========", "0123456789"]
+
+tally :: [Integer] -> [Int]
+tally xs = [countInstances i | i <- [0..9]]
+  where countInstances x = length (filter (== x) xs)
