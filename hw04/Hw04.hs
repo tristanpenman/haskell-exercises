b299da257c7d98df1c006becc6654a8fdeef45cb
Tristan Penman <tristan@tristanpenman.com>
2020-02-26 10:41:56 +1100

    Add solution for part 2 of exercise 1 in week 4

--- hw04/Hw04.hs
+++ hw04/Hw04.hs
@@ -5,6 +5,8 @@ module Hw04 where
 --
 -- Exercise 1 - Wholemeal programming
 --
+-- Part 1
+--
 
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
@@ -45,4 +47,55 @@ fun1'3 :: [Integer] -> Integer
 fun1'3 xs = foldr (*) 1 (map (\x -> x - 2) (filter even xs))
 
 fun1' :: [Integer] -> Integer
-fun1' = foldr (*) 1 . map (subtract 2) . filter even
+fun1' = foldr (*) 1 . map ((-) 2) . filter even
+
+--
+-- Part 2
+--
+-- Hint: For this problem you may wish to use the functions iterate and
+-- takeWhile. Look them up in the Prelude documentation to see what they do.
+--
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n    = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+--
+-- Expanding out for an example:
+--
+-- fun2 3
+--     = fun2 (3 * 3 + 1)
+--     = fun2 10
+--     = 10 + fun2 5
+--     = 10 + fun2 (3 * 5 + 1)
+--     = 10 + fun2 16
+--     = 10 + (16 + fun2 8)
+--     = 10 + (16 + (8 + fun2 4))
+--     = 10 + (16 + (8 + (4 + fun 2)))
+--     = 10 + (16 + (8 + (4 + 2 + fun 1)))
+--     = 10 + (16 + (8 + (4 + 2 + 0)))
+--     = 40
+--
+-- Notes: A value for `n` is only added to the sum when it is even; odd numbers 'fall through',
+-- but become even... since `iterate` will return all numbers, we can filter them out.
+--
+-- First, try combining `takeWhile` and `iterate`:
+--
+
+fun2'1 :: Integer -> [Integer]
+fun2'1 = takeWhile (/= 1) . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
+
+--
+-- fun2'1 3 = [3,10,5,16,8,4,2]
+--
+-- (sum . filter even . fun2'1) 3 = 40
+--
+
+fun2' :: Integer -> Integer
+fun2' = sum
+      . filter even
+      . takeWhile (/= 1)
+      . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
+
