6b13d18ead097964a303130eda777d48537945ef
Tristan Penman <tristan@tristanpenman.com>
2020-02-26 10:42:22 +1100

    Add problem definition for exercise 2 in week 4

--- hw04/Hw04.hs
+++ hw04/Hw04.hs
@@ -99,3 +99,20 @@ fun2' = sum
       . takeWhile (/= 1)
       . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
 
+--
+-- Exercise 2 - Folding with trees
+--
+-- Given a definition of a tree...
+--
+
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+--
+-- define a function which generates a balanced binary tree from a list of
+-- values using foldr:
+--
+
+foldTree :: [a] -> Tree a
+foldTree = undefined
