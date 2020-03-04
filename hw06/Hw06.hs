7bc76be6b0c593da320143c4cd42e48e7d794adb
Tristan Penman <tristan@tristanpenman.com>
2020-03-04 23:18:06 +1100

    Add solutions for exercises 1-4 for week 6

---
+++ hw06/Hw06.hs
@@ -0,0 +1,93 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Hw06 where
+
+--
+-- Exercise 1
+--
+-- Come up with
+--
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+--
+-- Exercise 2
+--
+-- Now come up with more a efficient implementation.
+--
+
+fibs2 :: [Integer]
+fibs2 = let fibs2' a b = a : (fibs2' b (a + b))
+        in fibs2' 0 1
+
+fibs3 :: [Integer]
+fibs3 = let fibs3' a b = a : (fibs3' b $ a + b)
+        in fibs3' 0 1
+
+--
+-- Exercise 3
+--
+-- Define a data type of polymorphic streams, Stream.
+--
+-- Then write a function to convert a Stream to an infinite list:
+--
+
+data Stream a = Stream a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Stream x xs) = x : streamToList xs
+
+-- TODO: This can be improved
+instance Show a => Show (Stream a) where
+  show s = show (take 20 (streamToList s))
+
+--
+-- Exercise 4
+--
+
+--
+-- Part 1
+--
+-- Write a function:
+--
+--    streamRepeat :: a -> Stream a
+--
+-- which generates a stream containing infinitely many copies of the
+-- given element
+--
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Stream x (streamRepeat x)
+
+--
+-- Part 2
+--
+-- Write a function
+--
+--    streamMap :: (a -> b) -> Stream a -> Stream b
+--
+-- which applies a function to every element of a Stream.
+--
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap fn (Stream s xs) = Stream (fn s) (streamMap fn xs)
+
+--
+-- Part 3
+--
+-- Write a function
+--
+--    streamFromSeed :: (a -> a) -> a -> Stream a
+--
+-- which generates a Stream from a “seed” of type a, which is the first
+-- element of the stream, and an “unfolding rule” of type a -> a
+--
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed fn s = Stream s (streamFromSeed fn (fn s))
