c2af1411d6886aa6db2e17b789da59b26af480c1
Tristan Penman <tristan@tristanpenman.com>
2020-04-23 21:38:21 +1000

    Add parts 2 and 3 of exercise 2 in week 7

--- hw07/Hw07.hs
+++ hw07/Hw07.hs
@@ -25,6 +25,12 @@ tag (Append m _ _) = m
 --
 -- Exercise 2
 --
+-- Part 1
+--
+-- Write the function:
+--
+--    indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+--
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ 0 (Single _ a) = Just a
@@ -33,6 +39,42 @@ indexJ idx (Append _ jl1 jl2)
   | otherwise     = indexJ (idx - jl1Size) jl2
     where jl1Size = getSize (size (tag jl1))
 indexJ _ _ = Nothing
+
+--
+-- Part 2
+--
+-- Write the function:
+--
+--    dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+--
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n jl1@(Single _ _)
+  | n < 1 = jl1
+dropJ n (Append s jl1 jl2)
+  | n < jl1Size = (dropJ n jl1) +++ jl2
+  | n < totalSize = (dropJ (n - jl1Size) jl2)
+    where jl1Size = getSize (size (tag jl1))
+          totalSize = getSize (size s)
+dropJ _ _ = Empty
+
+--
+-- Part 3
+--
+-- Write the function:
+--
+--     takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+--
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n jl1@(Single _ _)
+    | n > 0 = jl1
+takeJ n (Append _ jl1 jl2)
+    | n >= jl1Size = jl1 +++ (takeJ (n - jl1Size) jl2)
+      where jl1Size = getSize (size (tag jl1))
+takeJ _ _ = Empty
+
+-- Used for testing
 
 az :: JoinList Size Char
 az = foldr1 (+++) $ Single (Size 1) <$> ['a'..'z']
